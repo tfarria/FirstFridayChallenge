53d671ad9c975f884e2a14475094125096a8965a
Terry Farria <tfjteachus@gmail.com>
2018-03-30 15:44:22 -0400

    assignment complete with bonus

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,20 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        //Modify your program to allow you to change the ending number. Now your program shall print the numbers from 1 to the number.
+        //Once the last number has displayed then print the number of multiples of 7, 17 and 23.
 
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = keyboard.nextInt();
+        keyboard.nextLine();
 
-        for ( int a = 1; a<101; a++) {
+        for ( int a = 1; a<number; a++) {
 
             if (a % 5 ==0 && a% 3==0){
                 System.out.println("FizzBuzz" );
@@ -24,6 +32,19 @@ public class Main {
                 System.out.println(a);
             }
 
+        }
+
+
+        for (int b = 1; b<number; b++){
+            if (b % 7==0){
+                System.out.println(b + " is a multiple of 7. ");
+            }
+            else if (b % 17==0){
+                System.out.println(b + " is a multiple of 17. ");
+            }
+            else if (b % 23==0){
+                System.out.println(b + " is a multiple of 23. ");
+            }
         }
     }
 }
