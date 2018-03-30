fac137f5b5aa2df68863fe2ae1aff550b3b62e48
Terry Farria <tfjteachus@gmail.com>
2018-03-30 14:15:06 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        boolean done = false;
+
+
+
+
+
+
+
+
+
+        for ( int a = 1; a<101; a++) {
+            System.out.println(+a);
+
+
+
+
+
+
+
+        }
+    }
+}
